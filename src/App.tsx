bfcea9d1cbd31070f0ebc6e9dc497819e66905c8
bheshmat-rayane <76139517+bheshmat-rayane@users.noreply.github.com>
2025-07-07 14:30:48 -0700

    9

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/cashew-spark-reboot">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
